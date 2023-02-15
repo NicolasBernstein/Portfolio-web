444e4beb8de08feb2e0269a54a66121a1b80078a
Nicolas Daniel <nicoo.bernstein@gmail.com>
2023-02-15 19:04:04 -0300

    creacion de tarjetas de proyectos

--- index.js
+++ index.js
@@ -11,13 +11,30 @@ window.addEventListener('load', ()=>{
 
            console.log(element);
         var body = element.querySelector(".card-body");
-         body.style.bottom = `0rem`;
+        var imagecover = element.querySelector("#imagecover");
+        var buttons = element.querySelectorAll(".zindex")
+        var elementitle = element.querySelector(".elementitle");
+        elementitle.style = "opacity: 1;";
+        buttons.forEach(but =>{
+            but.style = "opacity: 1;";
+        })
+        imagecover.style = " background-color: rgba(111, 66, 193, 0.5); height: 108%";
+         body.style = `opacity: 1; bottom: -0.1rem;`;
+
 
         })
         element.addEventListener("mouseleave", ()=>{
             console.log("mouseout");
             var body = element.querySelector(".card-body");
-            body.style.bottom = `9rem`;
+            var imagecover = element.querySelector("#imagecover");
+            imagecover.style = "background-color: rgba(111, 66, 193, 0.5); height: 0%;";
+            var buttons = element.querySelectorAll(".zindex")
+            var elementitle = element.querySelector(".elementitle");
+            elementitle.style = "opacity: 0;";
+            buttons.forEach(but =>{
+                but.style = "opacity: 0;";
+            })
+            body.style = `opacity: 0; bottom: 9rem;`;
         })
     })
 })
