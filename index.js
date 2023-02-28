710837262666ae2c14777a215ea07ccc2ea5a36c
Nicoo <nicoo.bernstein@gmail.com>
2023-02-28 00:46:15 -0300

    carousel working

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 window.addEventListener('load', ()=>{
     const anim = [
 {bottom: '0rem'}
@@ -91,7 +93,43 @@ select.querySelector("div").addEventListener('click', ()=>{
 })
 
 
+function App(){}
+console.log("nice");
+    console.log("new");
+    var app = new App();
+    window.app = app
 
+
+
+    App.prototype.processingButton = function(event){
+        
+const btn = event.currentTarget;
+const carruselList = event.currentTarget.parentNode;
+const track = event.currentTarget.parentNode.querySelector("#track");
+const carrusel = track.querySelectorAll(".carusel");
+
+const carruselwidth = carrusel[0].offsetWidth;
+
+const trackWidth = track.offsetWidth;
+const listWidth = carruselList.offsetWidth;
+
+track.style == "" ? leftPosition = track.style.leftPosition = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+btn.dataset.button == "button-prev" ? prevAction(leftPosition, carruselwidth, track) : nextAction(leftPosition, trackWidth, listWidth, carruselwidth, track)
+
+
+    }
+    let prevAction = (leftPosition, carruselwidth, track) =>{
+        if(leftPosition > 0){
+            track.style.left = `${-1 * (leftPosition - carruselwidth)}px`;
+        }
+    }
+
+    let nextAction = (leftPosition, trackWidth, listWidth, carruselwidth, track) =>{
+        if(leftPosition < (trackWidth - listWidth)){
+            track.style.left = `${-1 * (leftPosition + carruselwidth)}px`;
+        }
+    }
 
 
 })
+
